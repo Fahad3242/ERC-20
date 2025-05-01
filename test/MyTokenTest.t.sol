c9acab35975b175e86667b4f17259d667aebcbca
mfahad <sheikhshaikhfahad@gmail.com>
2025-05-02 00:30:26 +0500

    testinggg

--- test/MyTokenTest.t.sol
+++ test/MyTokenTest.t.sol
@@ -1,4 +1,4 @@
-//SPDX License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
@@ -29,11 +29,11 @@ contract MyTokenTest is Test {
 
     }
 
-    function testmeBalance() public {
+    function testMeBalance() public {
         // Check the balance of the "me" address
         assertEq(STARTING_BALANCE, myToken.balanceOf(me));
     }
-
+ 
     function testAllowanceWorks() public {
         uint256 initialAllowance = 1000 ether; 
         //me approves you to spend tokens on me's behalf
@@ -48,5 +48,44 @@ contract MyTokenTest is Test {
         assertEq(myToken.balanceOf(you), transferAmount);
         assertEq(myToken.balanceOf(me), STARTING_BALANCE - transferAmount);
     }
+
+    function testTransfer() public {
+        uint256 transferAmount = 100 ether;
+
+        vm.prank(me);
+        myToken.transfer(you, transferAmount);
+
+        assertEq(myToken.balanceOf(you), transferAmount);
+        assertEq(myToken.balanceOf(me), STARTING_BALANCE - transferAmount);
+}
+
+
+    function testApproveAndAllowance() public {
+        uint256 allowanceAmount = 200 ether;
+
+        vm.prank(me);
+        myToken.approve(you, allowanceAmount);
+
+        uint256 allowance = myToken.allowance(me, you);
+        assertEq(allowance, allowanceAmount);
+}
+
+    function testRevertTransferInsufficientBalance() public {
+        uint256 transferAmount = STARTING_BALANCE + 1;
+
+        vm.prank(me);
+        vm.expectRevert();
+        myToken.transfer(you, transferAmount);
+}
+
+    function testRevertTransferFromWithoutApproval() public {
+        uint256 transferAmount = 50 ether;
+
+        vm.prank(you);
+        vm.expectRevert();
+        myToken.transferFrom(me, you, transferAmount);
+}
+
+
 }
     
